0dc64347b5b87004165890ddcaed2c01387da4b2
Codkashh <as3733542@gmail.com>
2025-01-28 22:14:46 +0530

    updation in code

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,11 @@ import  authRoutes from "./routes/auth.route.js"
 
 const app = express();
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes)
-
-app.get("/", function(req, res){
-    res.send("sir ji");
-});
-
-app.listen(3000, () => {
+app.use(express.json());// this will allow us to parse incoming request from re.body
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port: ", PORT);
 });
