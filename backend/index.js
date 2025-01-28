9bddd8c90a5070e99e07e25845f3b22f2cc1c10f
Codkashh <as3733542@gmail.com>
2025-01-28 12:05:16 +0530

    DB connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import { connectDB } from "./db/connectDB.js"
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+app.use("/api/auth", authRoutes)
 
 app.get("/", function(req, res){
     res.send("sir ji");
-})
+});
 
 app.listen(3000, () => {
+    connectDB();
     console.log("Server is running on port 3000")
 });
