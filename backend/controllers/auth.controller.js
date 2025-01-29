c46dc2643a87a79b64b867f8bb78e1a91a41fd9e
Codkashh <as3733542@gmail.com>
2025-01-29 18:21:11 +0530

    debug controller code

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,52 +1,53 @@
-import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import crypto from "crypto";
+
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import { User } from "../models/user.model.js";
+
 export const signup = async (req, res) => {
-    const {email, password, name} = req.body;
-    try{
-        if(!email || !password || !name){
-            throw new Error("All fields are required");
-        }
+	const { email, password, name } = req.body;
 
-        const userAlreadyExists = await User.findOne({ email });
-        console.log("userAlreadyExists", userAlreadyExists);
-        if(userAlreadyExists) {
-            return res.status(400).json({
-                success:false, 
-                message:"User already exists"
-            });
-        }
-        const hashedPasswrd = await bcryptjs.hash(password, 10);
-        const verificationToken = 
-            Math.floor(100000 + Math.random() * 900000).toString();
+	try {
+		if (!email || !password || !name) {
+			throw new Error("All fields are required");
+		}
 
-        const user = new User({
-            email,
-            password: hashedPasswrd,
-            name,
-            verificationToken,
-            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000//24 hours
-        });
+		const userAlreadyExists = await User.findOne({ email });
+		console.log("userAlreadyExists", userAlreadyExists);
 
-        await user.save();
-        //JWT
-        generateTokenAndSetCookie(res, user._id);
+		if (userAlreadyExists) {
+			return res.status(400).json({ success: false, message: "User already exists" });
+		}
 
-        res.status(201).json({
-            success:false, 
-            message:"User created successfully",
-            user: {
-                ...user._doc,
-                password: undefined
-            },
-       });
+		const hashedPassword = await bcryptjs.hash(password, 10);
+		const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
 
-    } catch(error){
-        res.status(400).json({
-             success:false, 
-             message:error.message
-        })
-    };
+		const user = new User({
+			email,
+			password: hashedPassword,
+			name,
+			verificationToken,
+			verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, // 24 hours
+		});
+
+		await user.save();
+
+		// jwt
+		generateTokenAndSetCookie(res, user._id);
+
+		await sendVerificationEmail(user.email, verificationToken);
+
+		res.status(201).json({
+			success: true,
+			message: "User created successfully",
+			user: {
+				...user._doc,
+				password: undefined,
+			},
+		});
+	} catch (error) {
+		res.status(400).json({ success: false, message: error.message });
+	}
 };
 
 export const login = async (req, res) => {
--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,8 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // this will allow us to parse incoming request from re.body
 app.use("/api/auth", authRoutes)
-app.use(express.json());// this will allow us to parse incoming request from re.body
 app.listen(PORT, () => {
     connectDB();
     console.log("Server is running on port: ", PORT);
