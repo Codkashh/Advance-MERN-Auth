2d52753b60e774ec9b84eecec9689a98c8c4a231
Codkashh <as3733542@gmail.com>
2025-02-07 18:51:12 +0530

    check auth

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import { connectDB } from "./db/connectDB.js"
 import dotenv from "dotenv";
 import  authRoutes from "./routes/auth.route.js"
-
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // this will allow us to parse incoming request from re.body
+app.use(express.json());// this will allow us to parse incoming request from re.body
+app.use(cookieParser)// allow us to parse incoming cookies
 app.use("/api/auth", authRoutes)
 app.listen(PORT, () => {
     connectDB();
